cffc19b24dce1aae8da29fec60df4718ac74f0dd
ZonG <desharkushal5@gmail.com>
2025-02-24 14:51:52 +0545

    Initial commit

---
+++ worksheet-1/1.cpp
@@ -0,0 +1,32 @@
+//write a program that takes a tempreture value from the user.It should then allow the user to choose between Celsius(C) and Fahrenheit(F). 
+//After the user selection, it should then convert the enteredtemp to shoces scale and display the result.
+#include<iostream> 
+using namespace std;
+int main()
+{
+    char Temp;
+    float a, convertedTemp;
+    cout<<"what would you like to convert the tempreature into Celsius or Fahrenheit (C or F):";
+    cin>>Temp;
+    cout<<"Enter the tempreature:"<<endl;
+    cin>>a;
+    switch (Temp) {
+        case 'C':
+        case 'c': 
+            convertedTemp = (a - 32) * 5 / 9; 
+            cout << "The temperature in Celsius is: " << convertedTemp << "°C" << endl;
+            break;
+
+        case 'F':
+        case 'f': 
+            convertedTemp = (a * 9 / 5) + 32; 
+            cout << "The temperature in Fahrenheit is: " << convertedTemp << "°F" << endl;
+            break;
+
+        default: 
+            cout << "Invalid choice! Please enter 'C' or 'F'." << endl;
+            break;
+    }
+
+    return 0;
+}
