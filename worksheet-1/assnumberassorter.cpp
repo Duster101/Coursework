5402f9d787d9bce3870b7c87aa9b4b6354596a54
ZonG <desharkushal5@gmail.com>
2025-02-25 22:05:39 +0545

    eidted

---
+++ worksheet-1/assnumberassorter.cpp
@@ -0,0 +1,8 @@
+//write a program that reads an array of integer number from the user and sort the number in the assending order
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,n;
+    cout<<"Enter 7 numbers at random:";
+    cin>>a>>b>>c>>d>>e>>f>>g;
